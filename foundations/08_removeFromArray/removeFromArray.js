4f9df7d0f4e1019dc4ebd1a25f491d5ca3054db3
Katsudon27 <katsudon1127@gmail.com>
2026-04-06 15:01:55 +1000

    Completed exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...Args) {
+    for(let i = 0; i < Args.length; i++){
+        while(array.includes(Args[i])){
+            array.splice(array.indexOf(Args[i]), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
