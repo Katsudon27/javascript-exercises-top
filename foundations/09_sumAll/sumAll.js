d6938883d5221d4a0c25227110518f9c83ed85de
Katsudon27 <katsudon1127@gmail.com>
2026-04-06 20:49:53 +1000

    Completed exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a ,b) {
+    if (Number.isInteger(a) && Number.isInteger(b) && parseInt(a) >= 0 && parseInt(b) >= 0){
+        let sum, start, end;
+        const args = [...arguments];
 
+        args.sort(function (a,b) {return a - b;});
+
+        sum = args[0];
+        start = sum + 1;
+        end = args[1];
+
+        for (start; start <= end; start++){
+            sum += start;
+        }
+
+        return sum;
+    }else{
+        return("ERROR");
+    }
 };
 
 // Do not edit below this line
