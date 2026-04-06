44b03e31dd48a513a2c6e54c377e1b7a03561eea
Katsudon27 <katsudon1127@gmail.com>
2026-04-06 14:48:20 +1000

    Completed exercise 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(sentence) {
+    let reversedSentence = "";
+    let char = "";
 
+    for (let i = sentence.length; i > -1; i--){
+        char = sentence.charAt(i);
+        reversedSentence += char;
+    }
+
+    return reversedSentence;
 };
 
 // Do not edit below this line
