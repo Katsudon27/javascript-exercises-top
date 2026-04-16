a40afb12f550b924954b349d2002fa5f171e3bdb
Katsudon27 <katsudon1127@gmail.com>
2026-04-16 16:10:45 +1000

    Completed exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/[^a-z0-9]/gi, "").toLowerCase();
+    for (let i = 0; i < word.length; i++){
+        if (word[i] != word[word.length - 1 - i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
