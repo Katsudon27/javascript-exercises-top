b2c4ad1384b6c41deb4c6a8329208f7177651f4d
Katsudon27 <katsudon1127@gmail.com>
2026-04-17 10:47:55 +1000

    Completed exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let result = people.reduce((previous, current) => {
+        let currentAge;
+        if (!current.yearOfDeath){
+            currentAge = (new Date()).getFullYear() - current.yearOfBirth;
+        }else{
+            currentAge = current.yearOfDeath - current.yearOfBirth;
+        }
 
+        if (currentAge > previous.age){
+            previous.name = current.name;
+            previous.age = currentAge;
+        }
+
+        return previous;
+    }, { 
+        name: "",
+        age: 0,
+    });
+
+    return result;
 };
 
 // Do not edit below this line
