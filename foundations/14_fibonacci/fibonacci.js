4b517e3cbb45c13cd316790b702d6ec5918fde97
Katsudon27 <katsudon1127@gmail.com>
2026-04-17 10:10:41 +1000

    Completed exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num < 0){
+        return "OOPS";
+    }
+    let sequence = [0, 1];
+    let index = 1;
+    let temp;
 
+    for (let i = 2; i <= num; i++){
+        temp = sequence[index] + sequence[index-1];
+        sequence.push(temp);
+        index++;
+    }
+
+    return sequence[num];
 };
 
 // Do not edit below this line
